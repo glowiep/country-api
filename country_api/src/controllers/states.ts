5484ec78e8698653fe1e796779c71e32f6a4f4b0
Gloria Lim <glorialimartt@gmail.com>
2024-03-16 11:20:01 -0400

    Add controllers folder

---
+++ country_api/src/controllers/states.ts
@@ -0,0 +1,5 @@
+export const getAllStates = (req, res) => {}
+export const getStateById = (req, res) => {}
+export const createState = (req, res) => {}
+export const updateState = (req, res) => {}
+export const deleteState = (req, res) => {}
