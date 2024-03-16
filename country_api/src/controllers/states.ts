5c264a548fd50c940029c7108fc4bb177c51afff
Gloria Lim <glorialimartt@gmail.com>
2024-03-16 11:29:56 -0400

    Add GET all states query

--- country_api/src/controllers/states.ts
+++ country_api/src/controllers/states.ts
@@ -1,6 +1,11 @@
 import prisma from "../db";
 
-export const getAllStates = (req, res) => {}
+// CRUD operations/queries for states here
+export const getAllStates = async (req, res) => {
+  const states = await prisma.state.findMany();
+
+  res.json({ data: states });
+}
 export const getStateById = (req, res) => {}
 export const createState = (req, res) => {}
 export const updateState = (req, res) => {}
