343e73fddda8908258a3d63a50d28e248f80113a
Gloria Lim <glorialimartt@gmail.com>
2024-03-16 12:46:30 -0400

    Add CRUD operations for states

--- country_api/src/controllers/states.ts
+++ country_api/src/controllers/states.ts
@@ -1,12 +1,85 @@
+// CRUD operations/queries for states here
 import prisma from "../db";
 
-// CRUD operations/queries for states here
-export const getAllStates = async (req, res) => {
+// GET /api/v1/states
+export const getAllStates = async (
+  req: any,
+  res: { json: (arg: { data: any[] }) => void }
+) => {
   const states = await prisma.state.findMany();
 
   res.json({ data: states });
-}
-export const getStateById = (req, res) => {}
-export const createState = (req, res) => {}
-export const updateState = (req, res) => {}
-export const deleteState = (req, res) => {}
+};
+
+// GET /api/v1/states/:id
+export const getStateById = async (
+  req: { params: { id: number } },
+  res: {
+    json: (arg0: {
+      data: {
+        id: number;
+        name: string;
+        code: string;
+        createdAt: Date;
+        updateAt: Date;
+      } | null;
+    }) => void;
+  }
+) => {
+  const id = Number(req.params.id);
+  const state = await prisma.state.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  res.json({ data: state });
+};
+
+// POST /api/v1/states
+export const createState = async (
+  req: { body: { name: string; code: string } },
+  res: {
+    json: (arg0: {
+      data: {
+        id: number;
+        name: string;
+        code: string;
+        createdAt: Date;
+        updateAt: Date;
+      };
+    }) => void;
+  }
+) => {
+  const state = await prisma.state.create({
+    data: {
+      name: req.body.name,
+      code: req.body.code,
+    },
+  });
+
+  res.json({ data: state });
+};
+
+// PUT /api/v1/states/:id
+export const updateState = async (req: { params: { id: any; }; body: any; }, res: { json: (arg0: { data: { id: number; name: string; code: string; createdAt: Date; updateAt: Date; }; }) => void; }) => {
+  const id = Number(req.params.id);
+
+  const state = await prisma.state.update({
+    where: {id},
+    data: req.body
+  });
+
+  res.json({ data: state });
+};
+
+// POST /api/v1/states/:id
+export const deleteState = async (req: { params: { id: any; }; }, res: { json: (arg0: { data: { id: number; name: string; code: string; createdAt: Date; updateAt: Date; }; }) => void; }) => {
+  const id = Number(req.params.id);
+
+  const state = await prisma.state.delete({
+    where: {id}
+  })
+  
+  res.json({ data: state });
+};
--- country_api/src/routers/stateRouter.ts
+++ country_api/src/routers/stateRouter.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import { 
+  getAllStates,
+  getStateById,
+  createState,
+  updateState,
+  deleteState
+} from '../controllers/states';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({ msg: 'List of states'});
-});
+router.get('/', getAllStates);
+router.get('/:id', getStateById);
+router.post('/', createState);
+router.put('/:id', updateState)
+router.delete('/:id', deleteState)
 
 export default router;
